e0df678691e9013749fd18cea35cc248d7a8763e
Samtaupe <145565941+Samtaupe@users.noreply.github.com>
2024-06-06 21:35:26 +0000

    changement nom + séparation js

--- webGL/script.js
+++ webGL/script.js
@@ -1,73 +1,187 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// Création de la scène et du renderer
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x7d8d9f);
+			import Stats from 'three/addons/libs/stats.module.js';
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.getElementById('webGL-container'),
-  antialias: true
-});
+			import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+			import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+			import { Water } from 'three/addons/objects/Water.js';
+			import { Sky } from 'three/addons/objects/Sky.js';
 
-// Chargement de l'image
-const loader = new THREE.TextureLoader();
-const texture = loader.load('images/background.jpg');
-const imageTexture = loader.load('img.jpg');
+			let container, stats;
+			let camera, scene, renderer;
+			let controls, water, sun, mesh;
+
+			init();
+
+			function init() {
+
+				container = document.getElementById( 'container' );
+
+				//
+
+				renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.setAnimationLoop( animate );
+				renderer.toneMapping = THREE.ACESFilmicToneMapping;
+				renderer.toneMappingExposure = 0.5;
+				container.appendChild( renderer.domElement );
+
+				//
+
+				scene = new THREE.Scene();
+
+				camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 20000 );
+				camera.position.set( 30, 30, 100 );
+
+				//
+
+				sun = new THREE.Vector3();
+
+				// Water
+
+				const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+
+				water = new Water(
+					waterGeometry,
+					{
+						textureWidth: 512,
+						textureHeight: 512,
+						waterNormals: new THREE.TextureLoader().load( 'textures/water.jpg', function ( texture ) {
+
+							texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+						} ),
+						sunDirection: new THREE.Vector3(),
+						sunColor: 0xffffff,
+						waterColor: 0x001e0f,
+						distortionScale: 3.7,
+						fog: scene.fog !== undefined
+					}
+				);
+
+				water.rotation.x = - Math.PI / 2;
+
+				scene.add( water );
+
+				// Skybox
+
+				const sky = new Sky();
+				sky.scale.setScalar( 10000 );
+				scene.add( sky );
+
+				const skyUniforms = sky.material.uniforms;
+
+				skyUniforms[ 'turbidity' ].value = 10;
+				skyUniforms[ 'rayleigh' ].value = 2;
+				skyUniforms[ 'mieCoefficient' ].value = 0.005;
+				skyUniforms[ 'mieDirectionalG' ].value = 0.8;
+
+				const parameters = {
+					elevation: 2,
+					azimuth: 180
+				};
+
+				const pmremGenerator = new THREE.PMREMGenerator( renderer );
+				const sceneEnv = new THREE.Scene();
+
+				let renderTarget;
+
+				function updateSun() {
+
+					const phi = THREE.MathUtils.degToRad( 90 - parameters.elevation );
+					const theta = THREE.MathUtils.degToRad( parameters.azimuth );
+
+					sun.setFromSphericalCoords( 1, phi, theta );
+
+					sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
+					water.material.uniforms[ 'sunDirection' ].value.copy( sun ).normalize();
+
+					if ( renderTarget !== undefined ) renderTarget.dispose();
+
+					sceneEnv.add( sky );
+					renderTarget = pmremGenerator.fromScene( sceneEnv );
+					scene.add( sky );
+
+					scene.environment = renderTarget.texture;
+
+				}
+
+				updateSun();
+
+				//
+
+				const floor = new THREE.BoxGeometry(100, 10, 50 );
+				const material = new THREE.MeshStandardMaterial({
+                    roughness: 0,
+                    metalness: 0,
+                    shadowSide: THREE.DoubleSide
+                });
+
+				mesh = new THREE.Mesh( floor, material );
+
+				scene.add( mesh );
+                
+                const wall = new THREE.BoxGeometry(100, 50, 10);
+                const shadow = new THREE.ShadowMaterial();
+                mesh = new THREE.Mesh( wall, material);
+
+                mesh.position.set(0,0,-20)
+
+                scene.add(mesh);
+				//
+
+				controls = new OrbitControls( camera, renderer.domElement );
+				controls.maxPolarAngle = Math.PI * 0.495;
+				controls.target.set( 0, 10, 0 );
+				controls.minDistance = 40.0;
+				controls.maxDistance = 200.0;
+				controls.update();
+
+				//
+
+				stats = new Stats();
+				container.appendChild( stats.dom );
+
+				// GUI
+
+				const gui = new GUI();
+
+				const folderSky = gui.addFolder( 'Sky' );
+				folderSky.add( parameters, 'elevation', -90, 90, 0.1 ).onChange( updateSun );
+				folderSky.add( parameters, 'azimuth', - 180, 180, 0.1 ).onChange( updateSun );
+				folderSky.open();
 
 
-// Création d'un plan avec la texture de l'image
-const geometry = new THREE.PlaneGeometry(1000, 100);
-const material =  new THREE.MeshBasicMaterial({ map: texture });
-const plane = new THREE.Mesh(geometry, material);
-scene.add(plane);
+				//
 
-// Création de la lumière
-const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(5, 5, 5);
-light.intensity = 2;
-scene.add(light);
+				window.addEventListener( 'resize', onWindowResize );
 
-// Création de la caméra
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function animate() {
+
+				render();
+				stats.update();
+
+			}
+
+			function render() {
+
+				const time = performance.now() * 0.001;
 
 
-// Création des contrôles de mouvement
-const controls = new OrbitControls( camera, renderer.domElement );
-camera.position.set( 0, 20, 100 );
-controls.update();
+				water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
 
+				renderer.render( scene, camera );
 
-
-// Ajout des événements de mouvement
-document.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'ArrowUp':
-      camera.position.z -= 0.1;
-      break;
-      case 'ArrowDown':
-        camera.position.z += 0.1;
-        break;
-        case 'ArrowLeft':
-          camera.position.x -= 0.1;
-          break;
-          case 'ArrowRight':
-            camera.position.x += 0.1;
-            break;
-  }
-});
-// Boucle de rendu
-function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  renderer.render(scene, camera);
-}
-animate();
-
-// Ajout du rendu à la page web
-const canvas = document.getElementById('webGL-container');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-renderer.setSize(canvas.width, canvas.height);
-
-animate();
+			}
